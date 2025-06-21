0575d3ab617d4e70a3100788eaa0107234f11f21
te99y <f74106246@gs.ncku.edu.tw>
2025-06-22 01:15:33 +0800

    Tested matmul

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import AutoDiff as ad
 import numpy as np
 
 if __name__ == '__main__':
-
     # a = ad.array([[3, 2, 1], [10, 20, 30]], outer_shape=(3, 1,))
     # print(f'a.value : {a.value}')
     # print(f'a.shape : {a.shape}')
@@ -17,24 +16,58 @@ if __name__ == '__main__':
     # print(f'b.check shape : {b.check_shape()}')
     # print()
 
-    a = ad.array([[1, 2, 3], [11, 12, 13]])
-    b = ad.array([[[-9, -1, -9], [-9, -1, -9], [-9, -1, -9]]])
-
-    t1 = ad.tensor(a)
-    t2 = ad.tensor(b)
-
-    print(t1.abs())
-    print(t2.abs())
+    # a = ad.array([[1, 2, 3], [11, 12, 13]])
+    # b = ad.array([[[-9, -1, -9], [-9, -1, -9], [-9, -1, -9]]])
+    #
+    # t1 = ad.tensor(a)
+    # t2 = ad.tensor(b)
+    #
+    # print(t1.abs())
+    # print(t2.abs())
     # print(b @ a)
 
-    c = ad.array([[5, 0], [3, 1]], outer_shape=(2, 2, ))
-    d = ad.array([[3, 20], [-3, -2]] )
-    print(c)
-    print(d)
-    # print(ad.depth(0))
-    print(c @ d)
-
-    print(np.array(c.value) @ np.array(d.value))
+    np.random.seed(0)
+    l = np.random.randint(low=1, high=6)
+    size = np.random.randint(low=1, high=5, size=l)
+    [n, k, m] = np.random.randint(low=1, high=5, size=3)
+    c = ad.array(np.random.rand(*size, n, k).tolist())
+    d = ad.array(np.random.rand(*size, k, m).tolist())
+    # c = ad.array([[300.38320773, -319.54941813, -463.56006039, -342.67056251, -115.50972527, 99.65008376, -92.66947929,
+    #                -258.66999994],
+    #               [348.27260333, -238.07690764, -285.38546076, -371.71087571, 427.15175295, -220.25261214,
+    #                -498.92294765, -321.72106065],
+    #               [339.16841535, 471.93951976, 346.20681176, -188.24184105, 410.12903769, -334.46931805, 98.10095229,
+    #                -94.29076193],
+    #               [143.92434899, -209.30501581, 238.22961315, 63.27208654, -364.49817926, -443.29309457, 104.03532852,
+    #                272.72285255]])
+    # d = ad.array([[112.93923888, 167.73989173, 251.91529553, 311.47921457, 360.97651274, 348.05459086, 93.05462098,
+    #                483.44348472],
+    #               [234.03233164, 63.04080978, 300.22546738, 174.79073029, 244.84752549, 460.18327631, 457.1559201,
+    #                405.32965854],
+    #               [291.5257906, 418.59462239, 200.18691929, 192.74552789, 48.07161118, 303.49283775, 6.86419619,
+    #                174.37188119],
+    #               [457.45511254, 187.88592825, 156.5836226, 357.7283956, 161.68338363, 146.59457066, 101.09833252,
+    #                93.6738532]])
+    # c = ad.array(np.random.randint(low=-1, high=1, size=(4, 4, 4)).tolist())
+    # d = ad.array(np.random.randint(low=-1, high=1, size=(4, 8)).tolist())
+    np_mat = np.array(c.value) @ np.array(d.value)
+    ad_mat = c @ d
+    # np_mat = np.array(c.value) + np.array(d.value)
+    # ad_mat = c + d
+    # ad_mat = ad.array(ad.binary_elementwise(c.value, d.value, operator.add))
+    print(f'c: {c}')
+    print(f'd: {d}')
+    print(f'np_mat.shape: {np.shape(np_mat)}')
+    print(f'ad_mat.shape: {ad_mat.check_shape()}')
+    print('np mat:')
+    # print(np_mat)
+    print()
+    print('ad mat:')
+    # print(ad_mat.value[1])
+    print()
+    print(f'np=ad: {np.array(ad_mat.value, dtype=np.float64).tolist() == np_mat.tolist()}')
+    print(np.allclose(np.array(ad_mat.value), np_mat))
+    print()
 
     # y = ((t1 - t2).exp() - t3.abs().log()) + 2*t3
     # y = ((t1 - t2).log()).neg().abs()
@@ -102,6 +135,3 @@ if __name__ == '__main__':
     # cNP = np.matmul(aNP, np.transpose(aNP))
     # cNP = np.matmul(aNP, np.transpose(aNP))
     # print(list(cNP.flatten()) == c.flatten())
-
-
-
