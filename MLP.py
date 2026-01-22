890e5bb24b5d641294e19a2f83b4ea176ff0c67b
te99y <f74106246@gs.ncku.edu.tw>
2026-01-22 15:47:42 +0800

    clean MLP

--- MLP.py
+++ MLP.py
@@ -1,28 +1,17 @@
 import random
+import AutoDiff as ad
 
 
 class MLP:
-    def __init__(self, shape, activation):
-        if not isinstance(shape, list):
-            raise TypeError('Shape must be either list or tuple')
-        if len(shape) < 1:
-            raise ValueError('Must have at least 1 layer')
-        if any(u < 1 for u in shape):
-            raise ValueError('Must have at least 1 unit in each layer')
-        self.shape = shape
-        self.activation = activation
+    def __init__(self, dims: list | tuple, activation):
+        if any(u < 1 for u in dims): raise ValueError('Must have >= 1 unit in each layer')
+        if not callable(activation): raise ValueError(f'Expects activation to be callable, got {activation}.')
 
-        self.weights = []  # len = layers-1, input layer needs no weight
-        self.bias = []
-        self.weights_grad = []
-        self.bias_grad = []
-        for d_in, d_out in zip(shape[:-1], shape[1:]):
-            # we use inner product : prev_activation[1 x d_in] * weight_of_a_neuron[d_in x 1]
-            # since the weight of a neuron is a column vector[d_in x 1]
-            # and we have d_out neurons in a layer
-            # so the weight of a layer is a matrix[d_in x d_out]
-            self.weights.append([[random.gauss(mu=0.0, sigma=1.0) for _ in range(d_out)] for _ in range(d_in)])
-            self.bias.append([0.0]*d_out)
+        self.dims = dims
+        self.activation = activation
+        # A handy tensor constructor  will be good
+        self.weights = [ad.tensor(ad.array(random.uniform(-1.0, 1.0), (din, dout))) for din, dout in zip(dims, dims[1:])]
+        self.bias = [ad.tensor(ad.array(random.uniform(-1.0, 1.0))) for _ in dims[1:]]
 
     def gradient(self):
         pass
