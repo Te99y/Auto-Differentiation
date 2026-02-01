2c0b380430476acc52cbbbd0eae58b1be50f4206
te99y <f74106246@gs.ncku.edu.tw>
2026-02-01 12:58:36 +0800

    Added some APIs as chatgpt suggested

---
+++ autodiff/api.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import Any
+
+from .array import array
+from .tensor import tensor
+
+
+def as_tensor(x: Any) -> tensor:
+    return x if isinstance(x, tensor) else tensor.const(x)
+
+
+def as_array(x: Any) -> array:
+    return x if isinstance(x, array) else array(x)
