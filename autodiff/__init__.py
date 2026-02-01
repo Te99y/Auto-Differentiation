7e4738abb2433bca60136f66413a8e5ba9411449
te99y <f74106246@gs.ncku.edu.tw>
2026-02-01 13:01:23 +0800

    Added reshape to __all__

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -2,12 +2,12 @@ from .array import array
 from .tensor import tensor
 
 from .ops import add, sub, mul, div, neg, abs_, exp, log, sin, cos
-from .linalg import matmul, transpose, swapaxes, flatten
+from .linalg import flatten, reshape, transpose, swapaxes, matmul
 from .ad import jvp, vjp
 
 __all__ = [
     "array", "tensor",
     "add", "sub", "mul", "div", "neg", "abs_", "exp", "log", "sin", "cos",
-    "matmul", "transpose", "swapaxes", "flatten",
+    "flatten", "reshape", "transpose", "swapaxes", "matmul",
     "jvp", "vjp",
 ]
